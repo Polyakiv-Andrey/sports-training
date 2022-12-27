2d3632dcbe8bfac5421c1d96755e20bb331e9043
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-12-27 14:50:21 +0200

    added to sidebar links to created pages and installed debug-toolbar

--- SportsTraining/urls.py
+++ SportsTraining/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gym.urls', namespace='gym'))
+    path('', include('gym.urls', namespace='gym')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

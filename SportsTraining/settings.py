76209746f1335c30b84c59465bc4b332ca3207fd
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2023-01-01 17:37:51 +0200

    update settings & requirement.txt

--- SportsTraining/settings.py
+++ SportsTraining/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)s5@=dc0$@31^#xu)f#8wywcx0*zlmw0yrrc0sg+q%frp8d@e*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-)s5@=dc0$@31^#xu)f#8wywcx0*zlmw0yrrc0sg+q%frp8d@e*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -111,7 +111,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'gym.Athlete'
 
-LOGIN_REDIRECT_URL = '/gym/'
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
